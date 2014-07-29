73c0894f1b733398735eab1f66cf4a8f5b20a5d1
redrhino <support@redrhinofx.com>
2014-07-30 02:41:17 +0700

    chart control issue

---
+++ viaDDE/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace viaDDE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStartDDE = new System.Windows.Forms.Button();
+            this.cboSymbols = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(435, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 22);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "sell";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(315, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 22);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "buy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(435, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 22);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "close loss";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(315, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 22);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "close profit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(220, 8);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStartDDE
+            // 
+            this.btnStartDDE.Location = new System.Drawing.Point(139, 8);
+            this.btnStartDDE.Name = "btnStartDDE";
+            this.btnStartDDE.Size = new System.Drawing.Size(75, 23);
+            this.btnStartDDE.TabIndex = 10;
+            this.btnStartDDE.Text = "Start";
+            this.btnStartDDE.UseVisualStyleBackColor = true;
+            this.btnStartDDE.Click += new System.EventHandler(this.btnStartDDE_Click);
+            // 
+            // cboSymbols
+            // 
+            this.cboSymbols.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cboSymbols.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cboSymbols.FormattingEnabled = true;
+            this.cboSymbols.Location = new System.Drawing.Point(12, 10);
+            this.cboSymbols.Name = "cboSymbols";
+            this.cboSymbols.Size = new System.Drawing.Size(121, 21);
+            this.cboSymbols.TabIndex = 11;
+            this.cboSymbols.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(315, 111);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 199);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.DataSource = this.chart1.Annotations;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(9, 37);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 13;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 360);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cboSymbols);
+            this.Controls.Add(this.btnStartDDE);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStartDDE;
+        private System.Windows.Forms.ComboBox cboSymbols;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ viaDDE/Form1.cs
@@ -0,0 +1,437 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+using NDde.Client;
+
+namespace viaDDE
+{
+    public partial class Form1 : Form
+    {
+        public delegate void TickReceived(Tick tick);
+
+        public event TickReceived OnTick;
+
+        public string[] allSymbols = new string[]{
+            "EURUSD",
+            "USDCHF",
+            "USDJPY",
+            "GBPUSD",
+            "EURJPY",
+            "EURCHF",
+            "EURGBP",
+            "USDCAD",
+            "AUDUSD",
+            "GBPCHF",
+            "GBPJPY",
+            "CHFJPY",
+            "NZDUSD",
+            "EURCAD",
+            "AUDJPY",
+            "EURAUD",
+            "AUDCAD",
+            "NZDJPY"
+            };
+
+        private Dictionary<string, List<double>> dataSource = new Dictionary<string,List<double>>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var symbol in allSymbols)
+            {
+                dataSource.Add(symbol, new List<double>());// create database for each symbol and currentylu 19 Symbols 
+            }
+            TheContainer.TheForm = this;
+            this.OnTick += Form1_OnTick;
+           
+
+
+
+        }
+
+        void Form1_OnTick(Form1.Tick tick)
+        {
+            if (this.InvokeRequired)
+            {
+                // Execute the same method, but this time on the GUI thread
+                this.BeginInvoke(new TickReceived(Form1_OnTick), tick);
+
+                // we return immedeately
+                return;
+            }
+            
+            
+            
+            // A first chance exception of type 'System.InvalidOperationException' occurred in System.Windows.Forms.dll
+            //Additional information: Cross-thread operation not valid: Control 'cboSymbols' accessed from a thread other than the thread it was created on.
+            // Control.CheckForIllegalCrossThreadCalls Property == false will fix 
+            //'TheContainer.TheForm.cboSymbols.SelectedItem' threw an exception of type 'System.InvalidOperationException'
+            if ((string)TheContainer.TheForm.cboSymbols.SelectedItem == tick.symbol)
+            {   
+                // Only 1 Chart -- Is tick Match 
+               // Log(tick.Bid)
+                // arrays #, datasource string 
+                Log(tick.bid.ToString());
+                chart1.Update();
+                chart1.DataBind();
+                
+            }
+        }
+
+        private void eventLog1_EntryWritten(object sender, System.Diagnostics.EntryWrittenEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //------------------JULY 2014 ------------------------
+            // Form1_Load event handler to initialize the Chart   ||--------------------- TEST 1
+            //----------------------------------------------------
+          //  this.princetonTempTableAdapter.Fill(this.pt1.PrincetonTemp);
+        /*
+            DataTable dtable=new DataTable();
+           
+          //  dtable = pt1.PrincetonTemp;
+          
+           // chart1.DataSource = bindingSource1;
+       
+            chart1.Series["Series1"].XValueMember =
+         
+            Convert.ToString(dtable.Columns[1]);
+         
+            chart1.Series["Series1"].YValueMembers =
+         
+            Convert.ToString(dtable.Columns[2]);
+         
+            chart1.Series["Series2"].XValueMember =
+        
+            Convert.ToString(dtable.Columns[1]);
+        
+            chart1.Series["Series2"].YValueMembers =
+         
+            Convert.ToString(dtable.Columns[3]);
+         
+            chart1.DataBind();
+         
+            chart1.Visible = true;
+         */
+          
+            //------------------JULY 2014 ------------------------
+            // Form1_Load event handler to initialize the Chart   ||--------------------- END TEST 1
+            //----------------------------------------------------
+            //---------THIS SHOULD WORK - JULY 2014-------
+            // Set palette.
+               this.chart1.Palette = ChartColorPalette.Bright;
+            // Set title.
+              this.chart1.Titles.Add("FX4BTC");
+            var series = new Series();
+            series.Name = "Series1";
+            series.ChartType = SeriesChartType.FastLine;
+            series.XValueType = ChartValueType.DateTime;
+            series.YValueType = ChartValueType.Double;// for Forex currency 12.90493 value
+
+            //------------------JULY 2014 ------------------------
+            // Form1_Load event handler to initialize the Chart  || --------------------- TEST 2 
+            //----------------------------------------------------
+            //--assign an array of strings (Series) and an array of integers (Points)
+            // set up some data
+           var xvals = new[]
+               {
+                  new DateTime(2012, 4, 4), 
+                  new DateTime(2012, 4, 5), 
+                   new DateTime(2012, 4, 6), 
+                   new DateTime(2012, 4, 7)
+               };
+           var yvals = new[] { 0.9050,0.90515,0.90520,0.90536 };
+            //-for-loop, we add the strings to the Series collection and add the integers to the Points collections on those Series.
+            // Data arrays.
+
+
+          
+
+            // Bind Data
+            //----------------------------------------------------
+            // xvals = Datetime, 
+            // yvals = bid price
+            //----------------------------------------------------
+            // Bind Data
+            chart1.Series["Series1"].Points.DataBindXY(xvals, yvals);
+            
+          //  chart1.DataSource = dataSource[(string)cboSymbols.SelectedItem];
+          //  chart1.DataBind(dataSource)
+
+            //------------------JULY 2014 ------------------------
+            // Form1_Load event handler to initialize the Chart  || ------------------------------------ End Test 2
+            //----------------------------------------------------
+            // Load DataSOurce
+
+            
+            // bind IListSource, the interface can be used for Data Table and Data Set classes.
+            //IList, the interface can be used for 1D arrays.
+            //IBindingList, the interface can be used for the generic Binding List class.
+            //IBindingListView, the interface can be used for Binding Source class.
+            //properties of DataSource and ValueMember to data binding
+
+
+
+
+            InitDDEClient();
+            cboSymbols.Items.AddRange(allSymbols);
+            cboSymbols.SelectedIndex = 0;
+
+            //------------------------------------------------------
+            // Add a button to allow the saving of the image for later use. 
+            //this.chart1.SaveImage("C:\\chart.png", ChartImageFormat.Png);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopDDE();
+        }
+        //--strategy
+        private void Log(string pText)
+        {
+            textBox1.AppendText(pText);
+            textBox1.AppendText("\r\n");
+        }
+
+        private void OnAdvise(object sender, DdeAdviseEventArgs args)
+        {
+            //=========================================================================================
+            //Log(args.Item + " " + args.Text); // get the DDE feed
+
+            string[] fxData = (args.Item + " " + args.Text).Split(new char[] { ' ' });
+
+            Tick tick = null;
+            
+            try{
+                tick = new Tick()
+                {
+                    //EURUSD 2014/05/06 23:59 1.34455 1.34456
+                    symbol = fxData[0],
+                    date = DateTime.Parse(fxData[1] + " " + fxData[2]),
+                    bid = double.Parse(fxData[3]),
+                    ask = double.Parse(fxData[4])
+                   
+
+                };   
+            }
+           
+            catch(Exception)
+            {
+                Log("Invalid tick data");
+            }
+
+            //-- Log Price -- works -- prints bid price for All symbol 
+           //   Log(fxData[3]); Double checked July 2014 - so data is local and present here, 
+            //-------------
+
+           
+
+
+           //------------------------------------------------
+            // Bind Data
+         //   chart1.Series["Series1"].Points.DataBindXY(fxData[1], fxData[3]);
+        //    chart1.Series["Series1"].Points.DataBindXY(fxData[2], fxData[3]);
+          //  chart1.Series["Series1"].Points.DataBind(dataSource, fxData[2], fxData[3],);
+
+        //    string xVal = fxData[2];// Hours:Minutes == 23:59
+        //    string YVal = fxData[3];
+
+         //   chart1.Series["Series1"].Points.DataBindXY(xVal, YVal);
+       //     chart1.Series["Series1"].Points.AddXY(xVal, YVal);
+
+         //   chart1.Series[0].Points.AddXY(date, bid);
+
+            //---------------------------------------------------
+            // create data source
+            if(tick != null)
+            {
+                if (dataSource.ContainsKey(tick.symbol))
+                {
+                    dataSource[tick.symbol].Insert(0, tick.bid);
+                 //   Log(tick.bid);
+                }
+                else
+                {
+                   dataSource.Add(tick.symbol, new List<double>(){ tick.bid });
+                }
+                if (OnTick != null)
+                {
+                    OnTick.Invoke(tick);
+                }
+            }
+           
+
+            //------------------------------
+
+        }
+
+        private void OnDisconnected(object sender, DdeDisconnectedEventArgs args)
+        {
+            Log(
+                "OnDisconnected: " +
+                "IsServerInitiated=" + args.IsServerInitiated.ToString() + " " +
+                "IsDisposed=" + args.IsDisposed.ToString());
+            TheContainer.TheDdeClient = null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //ctrader open api - Buy Send 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ctrader open api - Sell Send 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //ctrader open api - Find and Close All Profitable Positions
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //ctrader open api - Find and close All positions
+            //that are at a floating Loss more than COmmission Cost plus 1 pip
+        }
+
+        
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //user input - Select Curreny Pair for Renko Chart
+            if (!dataSource.ContainsKey((string)cboSymbols.SelectedItem))
+            {
+                dataSource.Add((string)cboSymbols.SelectedItem, new List<double>());
+            }
+            // Sets ut the database 
+            // Get and Set the dataSource for chart1 
+            // database contains all the contents from the public class TICK , which containts, symbol, bid, time
+            chart1.DataSource = dataSource[(string)cboSymbols.SelectedItem];
+
+            Log("Changed data source to " + cboSymbols.SelectedItem);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStartDDE_Click(object sender, EventArgs e)
+        {
+            // Button " Start"
+            ConnectToDDE();//works - debugger through
+            SubscribeAll();
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopDDE();
+        }
+
+
+        private void InitDDEClient()
+        {
+            DdeClient client = new DdeClient("MT4", "QUOTE");
+            TheContainer.TheDdeClient = client;
+
+            // Subscribe to the Disconnected event.  This event will notify 
+            // the application when a conversation has been terminated.
+            client.Disconnected += OnDisconnected;
+            client.Advise += OnAdvise;// ==================================================//
+        }
+
+        private void ConnectToDDE()
+        {
+            if (!TheContainer.TheDdeClient.IsConnected)
+            {
+                try
+                {
+                    TheContainer.TheDdeClient.Connect();
+                    Log("DDE Client Started");
+
+                }
+                catch (Exception)
+                {
+                    Log("An exception was thrown during DDE connection");
+                    Log("Ensure Metatrader 4 is running and DDE is enabled");
+                    Log("To activate the DDE Server go to Tools -> Options");
+                    Log("On the Server tab, ensure \"Enable DDE server\" is checked");
+                }
+            }
+            else
+            {
+                Log("Already connected");
+            }
+        }
+
+        private void SubscribeAll()
+        {
+            if (TheContainer.TheDdeClient != null && TheContainer.TheDdeClient.IsConnected)
+            {
+                var client = TheContainer.TheDdeClient;
+                foreach (var symbol in allSymbols)
+                {
+                    client.StartAdvise(symbol, 1, true, 60000);
+                }
+              
+            }
+        }
+
+       
+
+        private void StopDDE()
+        {
+            if (TheContainer.TheDdeClient != null && TheContainer.TheDdeClient.IsConnected)
+            {
+                TheContainer.TheDdeClient.Disconnect();
+            }
+
+            Log("Disconnected");
+            
+        }
+
+        public class Tick
+        {
+            public string symbol;
+            public double bid;
+            public double ask;
+            public DateTime date;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //---end strategy
+    }
+
+    // Not sure what the purpose of this..Singleton ?
+    // these static variables could be on Form1
+   
+    public class TheContainer
+    {
+        public static Form1 TheForm;
+        public static DdeClient TheDdeClient;
+    }
+
+}
